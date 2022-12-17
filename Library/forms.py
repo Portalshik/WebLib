f586222ff190f5048fb6f63230a7409056c461e2
Portalshik <61150089+Portalshik@users.noreply.github.com>
2022-12-18 00:40:34 +0300

    AddForm

--- Library/forms.py
+++ Library/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from .models import Tags
+
 
 class SearchForm(forms.Form):
     choiceList = (
@@ -9,3 +11,9 @@ class SearchForm(forms.Form):
     )
     search = forms.CharField(max_length=200)
     filter = forms.ChoiceField(choices=choiceList)
+
+
+class AddForm(forms.Form):
+    name_of_book = forms.CharField(max_length=200)
+    file = forms.FileField(widget=forms.ClearableFileInput)
+    tags = forms.CharField(widget=forms.Textarea)
--- Library/views.py
+++ Library/views.py
@@ -1,41 +1,41 @@
 from django.shortcuts import render
 
 from .models import Books, Categories, Authors
-from .forms import SearchForm
+from .forms import SearchForm, AddForm
 
 
 def index(request):
     if request.GET:
-        return render(request, 'index.html', {'search': search(request)})
+        data = search(request)
     else:
         data = Books.objects.all()
-        books = []
+    books = []
 
-        for i in data:
-            tags = []
-            if i.tag.count() >= 2:
-                for j in range(1, i.tag.count() + 1):
-                    tags.append(i.tag.get(id=j))
-            else:
-                tags.append(i.tag.get())
+    for i in data:
+        tags = []
+        if i.tag.count() >= 2:
+            for j in range(1, i.tag.count() + 1):
+                tags.append(i.tag.get(id=j))
+        else:
+            tags.append(i.tag.get())
 
-            authors = []
-            if i.author.count() >= 2:
-                for j in range(1, i.author.count() + 1):
-                    authors.append(i.author.get(id=j))
-            else:
-                authors.append(i.author.get())
+        authors = []
+        if i.author.count() >= 2:
+            for j in range(1, i.author.count() + 1):
+                authors.append(i.author.get(id=j))
+        else:
+            authors.append(i.author.get())
 
-            books.append({
-                'name': str(i.book_name),
-                'authors': authors,
-                'category': str(i.category),
-                'reissue': str(i.reissue_num),
-                'pub_date': str(i.publication_date),
-                'created_at': str(i.created_at),
-                'tags': tags
-            })
-        return render(request, 'index.html', {'books': books, 'form': SearchForm})
+        books.append({
+            'name': str(i.book_name),
+            'authors': authors,
+            'category': str(i.category),
+            'reissue': str(i.reissue_num),
+            'pub_date': str(i.publication_date),
+            'created_at': str(i.created_at),
+            'tags': tags
+        })
+    return render(request, 'index.html', {'books': books, 'form': SearchForm})
 
 
 def search(request):
@@ -47,3 +47,7 @@ def search(request):
             return Books.objects.get(book_name=req_search['search'])
         case 'author':
             return Books.objects.get(author=Authors.objects.get(author=req_search['search']))
+
+
+def add(request):
+    return render(request, 'add.html', {'form': AddForm})
